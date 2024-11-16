d6bc93d4515155005c13281db76a7d073ad7c8bb
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2024-11-17 03:54:34 +0800

    Added Command classes

---
+++ Command/ICommand.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../Config/GlobalConfig.h"
+#include "../Threading/IETThread.h"
+
+class ICommand
+{
+public:
+	enum CommandType {
+		PRINT
+	};
+
+	ICommand(int pid, CommandType commandType);
+	CommandType getCommandType();
+	virtual void execute();
+
+protected:
+	int pid;
+	CommandType commandType;
+};
+
+inline ICommand::CommandType ICommand::getCommandType()
+{
+	return this->commandType;
+}
+
+inline void ICommand::execute()
+{
+	std::cout << "Hello world from screen_" << this->pid << "!";
+}
